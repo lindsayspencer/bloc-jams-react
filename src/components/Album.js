da5f20a93b72109ed659852f11d57db2184bf97b
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2018-11-20 22:28:59 -0600

    Checkpoint Bloc Jams Components Library: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Album extends Component {
   render() {
     return(
-      <p className="album">This is the Album page.</p>
-    )
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
   }
 }
 
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
+// albumData is now an array that holds 2 objects - each object defines properties of a specific album
 
 class Library extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      albums: albumData
+    }
+  }
   render() {
     return(
-    <p className="library">This is the Library component.</p>
-  )}
+      <section className='library'>
+      { this.state.albums.map( (album, index) =>
+          <Link to={`/album/${album.slug}`} key={index}>
+          {album.title}
+          <img src={album.cover} alt={album.title} />
+          <div>{album.title}</div>
+          <div>{album.artist}</div>
+          <div>{album.songs.length} tracks</div>
+        </Link>
+      ) }
+        </section>
+       );
+  }
 }
 
 export default Library;
