eab6bbb19e0a36244ec284bd2d3537222e3f27f3
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2018-11-20 21:11:49 -0600

    Assignment Bloc Jams Routing: completed; created and added routing to Album page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing.js';
 import Library from './components/Library.js';
+import Album from './components/Album.js'
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album} />
         </main>
       </div>
     );
---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return(
+      <p className="album">This is the Album page.</p>
+    )
+  }
+}
+
+export default Album;
